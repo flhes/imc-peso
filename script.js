57162d332a129a9c8dadc511882c7630ffa78a38
FELIPE FELIX GAVIOLLE <00001106631547sp@aluno.educacao.sp.gov.br>
2024-06-04 13:54:40 -0300

    novo

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-alert("teste")
+function calcular(){
+    var fPeso = parseFloat(document.getElementById("peso"))
+    var fAltura = parseFloat(document.getElementById("altura"))
+    var fImc = fPeso /(fAltura*fAltura)
+    
+}
