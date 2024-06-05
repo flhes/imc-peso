21f93811fc749eae8efff1607e5c8502e0f8bc29
FELIPE FELIX GAVIOLLE <00001106631547sp@aluno.educacao.sp.gov.br>
2024-06-05 14:41:28 -0300

    final

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 function calcular(){
-    var fPeso = parseFloat(document.getElementById("peso"))
-    var fAltura = parseFloat(document.getElementById("altura"))
+    var fPeso = parseFloat(document.getElementById("peso").value)
+    var fAltura = parseFloat(document.getElementById("altura").value)
     var fImc = fPeso /(fAltura*fAltura)
-    
+    document.getElementById("imc").innerHTML = fImc.toFixed(2)
+
+    if(fImc<18.5){
+        document.getElementById("status").innerHTML = "STATUS: BAIXO PESO"
+    }
+    else if(fImc>=20.9){
+        document.getElementById("status").innerHTML = "STATUS: PESO IDEAL"
+
+    }
+    else if(fImc>29.9){
+        document.getElementById("status").innerHTML = "STATUS: SOBREPESO"
+    }
+    else if(fImc>34.9){
+        document.getElementById("status").innerHTML = "STATUS: OBESIDADE 1"
+
+    }
+    else if(fImc>39.9){
+        document.getElementById("status").innerHTML = "STATUS: OBESIDADE 2"
+    }
+    else{
+        document.getElementById("status").innerHTML = "STATUS: OBESIDADE MORBIDA"
+    }
 }
